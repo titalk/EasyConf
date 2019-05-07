58df9119072b8d7a74586030adb2970aa842571d
me <vicky606@zoho.com.cn>
2019-05-07 15:02:25 +0800

    Add features

---
+++ src/main/java/com/nuage/repository/HibernateSpeakerRepositoryImpl.java
@@ -0,0 +1,39 @@
+package com.nuage.repository;
+
+import com.nuage.model.Speaker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+@Repository("speakerRepository")
+@Profile("dev")
+public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+
+    @Autowired
+    private Calendar cal;
+
+    @Value("#{ T(java.lang.Math).random() * 100 }")
+    private double seedNum;
+
+    public List<Speaker> findAll() {
+        List<Speaker> speakers = new ArrayList<Speaker>();
+
+        Speaker speaker = new Speaker();
+
+        speaker.setFirstName("Bryan");
+        speaker.setLastName("Hansen");
+        speaker.setSeedNum(seedNum);
+
+        System.out.println("cal: " + cal.getTime());
+
+        speakers.add(speaker);
+
+        return speakers;
+    }
+
+}
