c107139f495315550e435e9d79b350bf4d759101
me <vicky606@zoho.com.cn>
2019-05-09 01:53:06 +0800

    More interestings

---
+++ src/main/java/com/nuage/model/Speaker.java
@@ -0,0 +1,32 @@
+package com.nuage.model;
+
+public class Speaker {
+
+    private String firstName;
+    private String lastName;
+    private double seedNum;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getSeedNum() {
+        return seedNum;
+    }
+
+    public void setSeedNum(double seedNum) {
+        this.seedNum = seedNum;
+    }
+}
